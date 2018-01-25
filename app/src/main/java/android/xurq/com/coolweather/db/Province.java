74e4b4fff42f54e3836a908daec82851ca01683d
xurunqing <xurq5953@gmail.com>
2018-01-25 17:40:44 +0800

    First commit.

---
+++ app/src/main/java/android/xurq/com/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package android.xurq.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by xu on 2018/1/25.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String proviceName;
+    private int privinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProviceName() {
+        return proviceName;
+    }
+
+    public void setProviceName(String proviceName) {
+        this.proviceName = proviceName;
+    }
+
+    public int getPrivinceCode() {
+        return privinceCode;
+    }
+
+    public void setPrivinceCode(int privinceCode) {
+        this.privinceCode = privinceCode;
+    }
+}
